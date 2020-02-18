d5150d7c94e521af7058254d76a5ce67b525e9aa
genekogan <kogan.gene@gmail.com>
2020-02-18 13:57:32 -0800

    helpers

--- src/Visuals/PointGeneration.h
+++ src/Visuals/PointGeneration.h
@@ -1,13 +1,57 @@
-//
-//  PointGeneration.hpp
-//  Visuals
-//
-//  Created by Gene Kogan on 2/17/20.
-//
+#pragma once
 
-#ifndef PointGeneration_hpp
-#define PointGeneration_hpp
+#include "ofMain.h"
+#include "ofxDelaunay.h"
 
-#include <stdio.h>
 
-#endif /* PointGeneration_hpp */
+//------------
+class PointGenerator
+{
+public:
+    void setBounds(float x, float y, float w, float h);
+    void setConvexBounds(vector<ofPoint> bounds);
+    virtual void addPoints(int n) { }
+    void clear() { points.clear(); }
+    vector<ofPoint> &getPoints() { return points; }
+protected:
+    ofPoint samplePointWithinBounds();
+    vector<ofPoint> bounds;
+    vector<ofPoint> points;
+};
+
+
+//------------
+class EvenPointsGenerator : public PointGenerator
+{
+public:
+    void addPoints(int n);
+    void setNumberCandidates(int numberCandidates);
+private:
+    int numberCandidates;
+};
+
+
+//------------
+class CirclePackGenerator : public PointGenerator
+{
+public:
+    void addPoints(int n);
+    void setMinDist(float minDist);
+private:
+    float minDist;
+};
+
+
+//------------
+class Triangulator
+{
+public:
+    void triangulate(vector<ofPoint> &points);
+    vector<ITRIANGLE> & getTriangles() { return triangles; }
+    vector<XYZI> & getVertices() { return vertices; }
+private:    
+    ofxDelaunay         triangulation;
+    vector<ITRIANGLE>   triangles;
+    vector<XYZI>        vertices;
+};
+
